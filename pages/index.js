d3cea01f76202a989b116a3ef027bc8a71c95c33
Danilo Catalan Canales <63259615+DCatCan@users.noreply.github.com>
2023-04-12 22:35:17 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,45 +13,47 @@ export default function Home() {
 	});
 	const [speciesOut, setSpeciesOut] = useState([]);
 	const [indeces, setIndeces] = useState([]);
-	function cleanse(x) {
-		//picks out the species that were chosen
-		const sharedGenomes = handler.getShared();
-		const temp = [];
-		x.forEach((element, index) => {
-			const state = element[1];
-			if (state) {
-				temp.push(sharedGenomes[index]);
-				setIndeces((elem) => [...elem, index]);
-			}
-		});
-		return temp;
-	}
+	
 
-	function rdyUp(speciesList) {
-		//removes any non shared genomes
-		const listSet = [];
-		let start;
-		if (speciesList.length > 0) {
-			start =
-				geneFilter.GeneSelected < geneFilter.maxGenes
-					? 0
-					: geneFilter.GeneSelected - Math.ceil(geneFilter.maxGenes / 2);
-
-			for (let index = 0; index < speciesList.length; index++) {
-				const ilement = speciesList[index];
-				let temp = [];
-				let jndex = start;
-				while (temp.length < geneFilter.maxGenes) {
-					temp.push(ilement[jndex]);
-					jndex++;
-				}
-
-				listSet.push(temp);
-			}
-		}
-		return listSet;
-	}
+	
 	useEffect(() => {
+		function cleanse(x) {
+			//picks out the species that were chosen
+			const sharedGenomes = handler.getShared();
+			const temp = [];
+			x.forEach((element, index) => {
+				const state = element[1];
+				if (state) {
+					temp.push(sharedGenomes[index]);
+					setIndeces((elem) => [...elem, index]);
+				}
+			});
+			return temp;
+		}
+		function rdyUp(speciesList) {
+			//removes any non shared genomes
+			const listSet = [];
+			let start;
+			if (speciesList.length > 0) {
+				start =
+					geneFilter.GeneSelected < geneFilter.maxGenes
+						? 0
+						: geneFilter.GeneSelected - Math.ceil(geneFilter.maxGenes / 2);
+	
+				for (let index = 0; index < speciesList.length; index++) {
+					const ilement = speciesList[index];
+					let temp = [];
+					let jndex = start;
+					while (temp.length < geneFilter.maxGenes) {
+						temp.push(ilement[jndex]);
+						jndex++;
+					}
+	
+					listSet.push(temp);
+				}
+			}
+			return listSet;
+		}
 		if (handler.getSpecies().length > 0) {
 			const a1 = cleanse(handler.getSpecies()); // get the relevant species selected;
 			const out = rdyUp(a1); // get the species rdy for display, shave away genomes that the speceis do not share
