bc67cf8f64be9c4c2f16199f728c17c976eed151
Danilo Catalan Canales <63259615+DCatCan@users.noreply.github.com>
2023-04-12 22:32:00 -0400

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -1,23 +1,36 @@
-import { useState } from 'react';
-import { Router, useRouter } from 'next/router'
-import ns from '@/styles/Nav.module.css'
+import { useState } from "react";
+import { Router, useRouter } from "next/router";
+import ns from "@/styles/Nav.module.css";
+import Link from "next/link";
 
 const Nav = (props) => {
-    const router = useRouter();
-    const handleClick = (e) =>{
-        e.preventDefault()
-        router.push(e.target.href)
-    }
-    return (
-        <>
-            <nav className={ns['nav']}>
-                <ul>
-                    <li><a href='/' onClick={handleClick}>Home</a></li>
-                    <li><a href='/userinput' onClick={handleClick}>Input</a></li>
-                </ul>
-            </nav>
-        </>
-    );
-}
+	const router = useRouter();
+	const handleClick = (e) => {
+		e.preventDefault();
+		router.push(e.target.href);
+	};
+	return (
+		<>
+			<nav className={ns["nav"]}>
+				<ul>
+					<li>
+						<Link
+							href="/"
+							onClick={handleClick}>
+							Home
+						</Link>
+					</li>
+					<li>
+						<Link
+							href="/userinput"
+							onClick={handleClick}>
+							Input
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		</>
+	);
+};
 
 export default Nav;
